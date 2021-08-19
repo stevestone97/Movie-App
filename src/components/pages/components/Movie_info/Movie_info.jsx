01aee7dd7bd3dab95ddaae5103627f4439ae40e6
stevestone <mohamadbraa97@gmail.com>
2021-08-19 09:59:50 +0200

    Added Style: MoviePage

--- src/components/pages/components/Movie_info/Movie_info.jsx
+++ src/components/pages/components/Movie_info/Movie_info.jsx
@@ -4,27 +4,42 @@ import Styles from "./Movie_info.module.css";
 function MovieInfo(props) {
   const detail = props.detail;
   const posterUrl = "https://image.tmdb.org/t/p/original/";
+  const hr = detail.runtime / 60;
+  const rhr = Math.round(hr);
+  const min = (hr - rhr) * 60;
+  const rmin = Math.round(min);
+  const userScore = detail.vote_average * 10;
+  const dotStyle = {
+    fontSize: "6px",
+    margin: "0 13px",
+  };
+  const ratingStyle =
+    userScore >= 70
+      ? { color: "green" }
+      : userScore >= 40
+      ? { color: "#FFC300" }
+      : { color: "#D90909" };
 
   return (
     <div className={Styles.poster}>
+      <img className={Styles.bg} src={posterUrl + detail.backdrop_path} />
       <div className={Styles.poster__description}>
         <img src={posterUrl + detail.poster_path} alt={detail.title} />
-        <div style={{ padding: "10px" }}>
-          <h1 className={Styles.title}>{detail.title}</h1>
+        <div className={Styles.details}>
+          <div className={Styles.title_date}>
+            <h1 className={Styles.title}>{detail.title}</h1>
+            <p className={Styles.date}>({detail.release_date?.slice(0, 4)})</p>
+          </div>
+          <div className={Styles.rating_genre}>
+            <p style={ratingStyle}>{`${userScore}%`}</p>
+            <p className={Styles.rating}>User Score</p>
+            <i style={dotStyle} class="fas fa-dot-circle" />
+            <p className={Styles.genre}>{props.genreList}</p>
+            <i style={dotStyle} class="fas fa-dot-circle" />
+            <p>{`${rhr}h ${rmin}m`}</p>
+          </div>
           <p className={Styles.overview}>{detail.overview}</p>
         </div>
-      </div>
-      <div className={Styles.col__info}>
-        <h3>Release date</h3>
-        <p>{detail.release_date}</p>
-        <h3>Run Time</h3>
-        <p>{detail.runtime}min</p>
-      </div>
-      <div className={Styles.col__info}>
-        <h3>IMDb Rating</h3>
-        <p>{detail.vote_average} /10</p>
-        <h3>Genres</h3>
-        <p className={Styles.genre}>{props.genreList}</p>
       </div>
     </div>
   );
